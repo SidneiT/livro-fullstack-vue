7b11326c845b94882362de98c46d62343ce003c0
Sidnei Teixeira <sidnei.teixeira@telefonica.com>
2019-12-06 19:19:17 -0300

    upvote finished

---
+++ 1-upvote/app/main.js
@@ -0,0 +1,60 @@
+const submissionComponent = {
+  template: `
+    <div style="display:flex; width:100%;">
+      <figure class="media-left">
+        <img
+          :src="submission.submissionImage"
+          class="image is-64x64"
+        />
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>
+              <a :href="submission.url" class="has-text-info">{{submission.title}}</a>
+              <span class="tag is-small">{{submission.id}}</span>
+            </strong>
+            <br />
+            {{submission.description}}
+            <br />
+            <small class="is-size-7">
+              Submitted by:
+              <img
+                :src="submission.avatar"
+                class="image is-24x24"
+              />
+            </small>
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <span class="icon is-small">
+          <i class="fa fa-chevron-up" @click="upvote(submission.id)"></i>
+          <strong class="has-text-info">{{submission.votes}}</strong>
+        </span>
+      </div>
+    </div>
+  `,
+  props: ["submission", "submissions"],
+  methods: {
+    upvote(submissionId) {
+      const submission = this.submissions.find(
+        submission => submission.id === submissionId
+      );
+      submission.votes++;
+    }
+  }
+};
+
+new Vue({
+  el: "#app",
+  components: { "submission-component": submissionComponent },
+  data: {
+    submissions: Seed.submissions
+  },
+  computed: {
+    sortedSubmissions() {
+      return this.submissions.sort((a, b) => b.votes - a.votes);
+    }
+  }
+});
